d32e58001801c3f97831dde1fbf6bb18a29ec3b1
Luke Larsen <luke@lukelarsen.com>
2025-03-26 13:39:46 -0600

    update eslint_d to not use outdates configs

--- nvim/lua/lukelarsen/plugins/linting.lua
+++ nvim/lua/lukelarsen/plugins/linting.lua
@@ -13,6 +13,26 @@ return {
 			python = { "pylint" },
 		}
 
+		-- Customize eslint_d configuration
+		lint.linters.eslint_d = {
+			name = "customlint",
+			-- Command to run eslint_d (override default if needed)
+			cmd = "eslint_d",
+			-- Arguments to pass to eslint_d
+			args = {
+				"--format",
+				"json", -- Ensure output is in JSON format for nvim-lint to parse
+				"--stdin",
+				"--stdin-filename",
+				function()
+					return vim.api.nvim_buf_get_name(0)
+				end, -- Pass current file name
+			},
+			-- Parse JSON output
+			stream = "stdout",
+			parser = require("lint.parser").json,
+		}
+
 		local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
 
 		vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
